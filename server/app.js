e2acd696e3e6229b84207f0178fcbb3dea121b6e
joshshelley <joshuashelley81@gmail.com>
2025-04-23 20:09:27 -0500

    refactor: modify mongo connection to utilize predefined variable

--- server/app.js
+++ server/app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 // Connect to the database
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .catch((err) => {
     console.error("Error occured attempting database connection", err);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -1,8 +1,5 @@
-const dotenv = require('dotenv');
 const app = require('./app');
 const { LOCALHOST } = require('./utility/constants');
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
