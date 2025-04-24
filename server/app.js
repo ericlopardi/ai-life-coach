8410c62fcb1b287031f37690c18a98e79e0d1540
joshshelley <joshuashelley81@gmail.com>
2025-04-23 22:07:40 -0500

    chore: cleanup

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const mongoose = require("mongoose");
 require("dotenv").config();
 const { connectToMongoDB } = require("./resource/mongo.js");
 
