4333d932d6f1f6a1f47594af615d84b1fb3b7c46
jshelley8117 <jshelley8117@gmail.com>
2025-05-02 21:08:15 -0500

    feat: introduce login page for UI

--- alc-ui/app/(tabs)/_layout.jsx
+++ alc-ui/app/(tabs)/_layout.jsx
@@ -6,6 +6,7 @@ export default function TabLayout() {
       <Tabs.Screen name="(goals)" />
       <Tabs.Screen name="(settings)" />
       <Tabs.Screen name="(journal)" />
+      <Tabs.Screen name="(auth)" />
     </Tabs>
   );
 }
