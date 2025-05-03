23651aa04561f72b1efa396a4e6172569b73e5cb
jshelley8117 <jshelley8117@gmail.com>
2025-05-02 22:05:19 -0500

    fix: eliminate stacked headers when navigating through deeply nested _layout.jsx files

--- alc-ui/app/(tabs)/_layout.jsx
+++ alc-ui/app/(tabs)/_layout.jsx
@@ -2,7 +2,7 @@ import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="(goals)" />
       <Tabs.Screen name="(settings)" />
       <Tabs.Screen name="(journal)" />
