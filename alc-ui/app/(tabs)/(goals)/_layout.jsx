d2542b49671e55618ad16b3961ca574ace4ae8db
jshelley8117 <103763460+jshelley8117@users.noreply.github.com>
2025-07-21 18:24:25 -0500

    SCRUM-25: Fix (NOBRIDGE) WARN [Layout children]: Too many screens defined. (#14)
    * refactor: remove unnecessary scren definitions to resolve NOBRIDGE WARN logs
    
    * feat: introduce token refresh logic in apiClient.js
    
    * refactor: fixing var reference because I suck
    
    * refactor: introduce caching mechanism for token refresh logic to reduce API call latency

--- alc-ui/app/(tabs)/(goals)/_layout.jsx
+++ alc-ui/app/(tabs)/(goals)/_layout.jsx
@@ -4,8 +4,6 @@ export default function TabLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Goals' }} />
-      <Stack.Screen name="new-entry" options={{ title: 'New Entry' }} />
-      <Stack.Screen name="entry/[id]" options={{ title: 'Entry' }} /> 
     </Stack>
   );
 }
